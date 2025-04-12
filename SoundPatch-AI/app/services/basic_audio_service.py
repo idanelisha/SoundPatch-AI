5db2af33584e8334a3a4992e3ff31de01f483a76
Idan Elisha <idanelisha9@gmail.com>
2025-04-12 19:57:57 +0300

    Delete SoundPatch-AI directory

--- SoundPatch-AI/app/services/basic_audio_service.py
+++
@@ -1,8 +0,0 @@
-from app.services.base_audio_service import BaseAudioService
-from app.models.config import AudioProcessingConfig
-from app.core.logging import logger
-
-class BasicAudioService(BaseAudioService):
-    def __init__(self, config: AudioProcessingConfig):
-        super().__init__(config)
-        logger.info("Initializing BasicAudioService") 
